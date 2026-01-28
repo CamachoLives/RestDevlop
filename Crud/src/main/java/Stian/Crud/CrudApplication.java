b83196e5d852b0c058fec59fa99b7c2c4b485d6f
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-01-27 19:23:04 -0500

    Another Activity

--- Crud/src/main/java/Stian/Crud/CrudApplication.java
+++ Crud/src/main/java/Stian/Crud/CrudApplication.java
@@ -2,17 +2,37 @@ package Stian.Crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+
 
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
 
-        final var Nombre = "Ubaldo Acosta Soto";
-        final var Empresa = "Global Mentoring";
-        final var Dominio = "com.mx";
+        var consola = new Scanner(System.in);
 
-        System.out.println("Email --> " + Nombre.toLowerCase().replace(" ", ".") + "@" + Empresa.toLowerCase().concat(".").replace(" ","") + Dominio);
+        System.out.println("** Recetas de Cocina **");
+
+        System.out.print("Ingresa el nombre de la receta: ");
+        var Name = consola.nextLine();
+
+        System.out.print("Ingresa los ingredientes: ");
+        var Ingredientes = consola.nextLine();
+
+        System.out.print("Ingresa el tiempo de preparación (Min): ");
+        var Time = consola.nextInt();
+
+        System.out.print("Ingresa la dificultad: ");
+        var Dificultad = consola.nextLine();
+
+
+        System.out.println("-- Receta de cocina --");
+        System.out.println("Nombre de la receta: " + Name);
+        System.out.println("Ingredientes: " + Ingredientes);
+        System.out.println("Tiempo de preparacion: " + Time);
+        System.out.println("Dificultad: " + Dificultad);
+
 
 		//SpringApplication.run(CrudApplication.class, args);
 	}
