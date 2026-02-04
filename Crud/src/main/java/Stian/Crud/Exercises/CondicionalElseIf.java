ab9a14de7c6463df47f08418ed8e0a4c6d19649c
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-02-04 15:29:14 -0500

    Exercise Sentence if else with virtual store

--- Crud/src/main/java/Stian/Crud/Exercises/CondicionalElseIf.java
+++ Crud/src/main/java/Stian/Crud/Exercises/CondicionalElseIf.java
@@ -1,15 +1,30 @@
 package Stian.Crud.Exercises;
 
+import java.util.Scanner;
+
 public class CondicionalElseIf {
     static void main() {
-        var age = 1;
+//      Reto tienda en linea
 
-        if (age >= 18){
-            System.out.println("You are adult");
-        }else if (age >= 13 && age < 18){
-            System.out.println("You are a child");
-        }else{
-            System.out.println("You are a kid");
+        var Consola = new Scanner(System.in);
+        System.out.println(" ** Tienda en linea ** ");
+        
+        System.out.println("Cual es el valor de la compra ?");
+        var value = Consola.nextInt();
+
+        System.out.println("Es miembro de la tienda ?");
+        var member = Consola.nextBoolean();
+        
+        if(value >= 1000 && member){
+            var Result = value - (value * 0.1);
+            System.out.println("Ha recibido el 10% de descuento por compra mayor a 1000 y por ser miembro, \n Valor total --> " + Result);
+        } else if (member) {
+            var Result = value - (value * 0.05);
+            System.out.println("Ha recibido el 5% de descuento por ser miembro, \n Valor total --> " + Result);
+        } else  {
+            System.out.println("No ha recibido nada de descuento porque no hizo compra mayor a 1000$ ni es miembro" + value);
         }
-}
+
+
+    }
 }
