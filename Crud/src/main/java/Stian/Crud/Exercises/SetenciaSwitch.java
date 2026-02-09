545502f6e2f57ecb376d2d36b54dfbe36b13bc13
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-02-08 21:09:40 -0500

    Sentence Switch with year

---
+++ Crud/src/main/java/Stian/Crud/Exercises/SetenciaSwitch.java
@@ -0,0 +1,53 @@
+package Stian.Crud.Exercises;
+
+public class SetenciaSwitch {
+    static void main() {
+//        int Day = 6;
+//
+//        switch (Day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Invalid Day!");
+//                break;
+//        }
+
+        int Month = 5;
+
+        switch(Month){
+            case 1 -> System.out.println("January");
+            case 2 -> System.out.println("February");
+            case 3 -> System.out.println("March");
+            case 4 -> System.out.println("April");
+            case 5 -> System.out.println("May");
+            case 6 -> System.out.println("June");
+            case 7 -> System.out.println("July");
+            case 8 -> System.out.println("August");
+            case 9 -> System.out.println("September");
+            case 10 -> System.out.println("October");
+            case 11 -> System.out.println("November");
+            case 12 -> System.out.println("December");
+            default -> System.out.println("Unknow Month");
+        }
+
+
+    }
+}
